5b39d50cc904863266899f74c0d91002b60ff242
vsuksi <vsuksi@proton.me>
2026-05-27 15:27:33 +0300

    Get multilevel variable from colData

--- R/utils.R
+++ R/utils.R
@@ -3,7 +3,8 @@
 #' @importFrom dplyr .data
 #' @import methods
 #' @importFrom notame drop_flagged drop_qcs combined_data flag merge_notame_sets
-#' mark_nas "flag<-" flag_quality log_text init_log finish_log citations
+#' @importFrom notame mark_nas "flag<-" flag_quality
+#' @importFrom notame log_text init_log finish_log citations
 #' @import SummarizedExperiment
 NULL
 
